958fe30f7af851d7d04dfe87697edc10b7161f36
Lentosy <bertdesaffel@gmail.com>
2017-04-15 18:58:28 +0200

    new structure

--- 1-9/problem4.pl
+++ 1-9/problem4.pl
@@ -1,8 +1,6 @@
 $max = 1000; # Each number of the product can not go higher than this number
 $largest = 0; #The largest palindrome that can be made with two 2-digit numbers.
 
-
-
 for ($i = 0; $i < $max; $i++){
   for($j = 0; $j < $max; $j++){
     $calc = $i * $j;
@@ -12,8 +10,6 @@ for ($i = 0; $i < $max; $i++){
 }
 
 print "$largest\n";
-
-
 
 sub is_palindrome {
   $n = @_[0];
