af611748bca0f1e61a6491d488babefe89424f50
bdsaffel <bertdesaffel@gmail.com>
2018-04-04 03:12:13 +0200

    remove unused code

--- 10-19/problem12.cpp
+++ 10-19/problem12.cpp
@@ -4,33 +4,12 @@
 #include <cmath>
 #include "../Utils/chrono.cpp"
 
-std::map<int, int> num_of_divisors_per_triangle_number;
-
-
-std::ostream& operator<<(std::ostream& out, const std::map<int, int> m){
-	std::map<int, int>::const_iterator it = m.begin();
-	while(it != m.end()){
-		std::cout << it->first << ": " << it->second << std::endl;
-		it++;
-	}
-}
-
 int get_number_of_divisors(int n){
 	int amount_of_divisors = 1;
 	for(int i = n/2; i > 0; i--){
-		//check if i already exists in the map
 		if(n%i == 0){
 			amount_of_divisors++;
 		}
-		/*
-		if(num_of_divisors_per_triangle_number.count(i) == 1){
-			std::cout << " + " <<  num_of_divisors_per_triangle_number[i];
-			amount_of_divisors += num_of_divisors_per_triangle_number[i];
-			break;
-		}else if(n % i == 0){
-			std::cout << " ++ ";
-			amount_of_divisors++;
-		}*/
 	}
 	
 	return amount_of_divisors;
@@ -49,9 +28,6 @@ int main(void){
 	}
 	
 	c.stop();
-	
-	
-	//std::cout << num_of_divisors_per_triangle_number;
 	
 	std::cout << current << " is the first triangle number to have over " << max_divisors << " divisors" << std::endl;
 	std::cout << "Found answer in " << c.time() << " seconds" << std::endl;
